fb7aebcbd1fd4336dfdf5fc2f8552b9194f2706e
Arashi005 <wamarda5@gmail.com>
2026-03-19 16:51:17 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,47 +1,13 @@
-const os = require('os');
-const settings = require('../settings.js');
+module.exports = {
+  name: "ping",
+  execute: async (sock, msg) => {
 
-function formatTime(seconds) {
-    const days = Math.floor(seconds / (24 * 60 * 60));
-    seconds = seconds % (24 * 60 * 60);
-    const hours = Math.floor(seconds / (60 * 60));
-    seconds = seconds % (60 * 60);
-    const minutes = Math.floor(seconds / 60);
-    seconds = Math.floor(seconds % 60);
+    const start = Date.now();
+    const end = Date.now();
 
-    let time = '';
-    if (days > 0) time += `${days}d `;
-    if (hours > 0) time += `${hours}h `;
-    if (minutes > 0) time += `${minutes}m `;
-    if (seconds > 0 || time === '') time += `${seconds}s`;
+    await sock.sendMessage(msg.key.remoteJid, {
+      text: `⚡ Speed: ${end - start}ms`
+    });
 
-    return time.trim();
-}
-
-async function pingCommand(sock, chatId, message) {
-    try {
-        const start = Date.now();
-        await sock.sendMessage(chatId, { text: 'Pong!' }, { quoted: message });
-        const end = Date.now();
-        const ping = Math.round((end - start) / 2);
-
-        const uptimeInSeconds = process.uptime();
-        const uptimeFormatted = formatTime(uptimeInSeconds);
-
-        const botInfo = `
-┏━━〔 🤖 𝐊𝐧𝐢𝐠𝐡𝐭𝐁𝐨𝐭-𝐌𝐃 〕━━┓
-┃ 🚀 Ping     : ${ping} ms
-┃ ⏱️ Uptime   : ${uptimeFormatted}
-┃ 🔖 Version  : v${settings.version}
-┗━━━━━━━━━━━━━━━━━━━┛`.trim();
-
-        // Reply to the original message with the bot info
-        await sock.sendMessage(chatId, { text: botInfo},{ quoted: message });
-
-    } catch (error) {
-        console.error('Error in ping command:', error);
-        await sock.sendMessage(chatId, { text: '❌ Failed to get bot status.' });
-    }
-}
-
-module.exports = pingCommand;
+  }
+};
