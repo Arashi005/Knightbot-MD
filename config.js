35c67e4fc51f7e071b751ba47325d92e721852d9
Arashi005 <wamarda5@gmail.com>
2026-03-19 16:39:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,13 @@
-require('dotenv').config();
+// ⚡ Voltaria Config
 
-global.APIs = {
-    xteam: 'https://api.xteam.xyz',
-    dzx: 'https://api.dhamzxploit.my.id',
-    lol: 'https://api.lolhuman.xyz',
-    violetics: 'https://violetics.pw',
-    neoxr: 'https://api.neoxr.my.id',
-    zenzapis: 'https://zenzapis.xyz',
-    akuari: 'https://api.akuari.my.id',
-    akuari2: 'https://apimu.my.id',
-    nrtm: 'https://fg-nrtm.ddns.net',
-    bg: 'http://bochil.ddns.net',
-    fgmods: 'https://api-fgmods.ddns.net'
-};
+global.botname = "Voltaria-MD"
+global.ownername = "Arashi"
+global.prefix = "."
+global.owner = ["254108720384"] // 
 
-global.APIKeys = {
-    'https://api.xteam.xyz': 'd90a9e986e18778b',
-    'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-    'https://api.neoxr.my.id': 'yourkey',
-    'https://violetics.pw': 'beta',
-    'https://zenzapis.xyz': 'yourkey',
-    'https://api-fgmods.ddns.net': 'fg-dylux'
-};
+global.footer = "⚡ Powered by Voltaria-MD"
+global.themeemoji = "⚡"
+global.location = "Kenya"
 
-module.exports = {
-    WARN_COUNT: 3,
-    APIs: global.APIs,
-    APIKeys: global.APIKeys
-};
+global.autoTyping = true
+global.autoRead = true
