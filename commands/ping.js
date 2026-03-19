0ac2699e4e48ff53d96a4ed11083d0e64942f61a
Arashi005 <wamarda5@gmail.com>
2026-03-19 16:56:17 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: "ping",
   execute: async (sock, msg) => {
+  const args = msg.body?.split(" ").slice(1).join(" ");
 
     const start = Date.now();
     const end = Date.now();
